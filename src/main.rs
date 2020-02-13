66ea7f4b1fe16cc77a7a4f60eea643b70791230f
alexis <alexis.auriac@epitech.eu>
2020-02-13 15:29:37 +0100

    The files in the cwd are now displayed in a Dialog with the cwd as its title

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::fs::DirEntry;
+use std::path::PathBuf;
 use std::{fs, io};
+
+use cursive::traits::*;
+use cursive::views::{Dialog, SelectView};
+use cursive::Cursive;
 
 fn get_entries(path: &str, hide: bool) -> Result<Vec<DirEntry>, io::Error> {
     let entries = fs::read_dir(path)?.collect::<Result<Vec<_>, io::Error>>()?;
@@ -18,15 +23,35 @@ fn get_entries(path: &str, hide: bool) -> Result<Vec<DirEntry>, io::Error> {
 }
 
 fn main() -> io::Result<()> {
-    // let mut siv = Cursive::default();
+    let pwd = PathBuf::from(".");
+    let pwd = fs::canonicalize(pwd)?;
 
-    let entries = get_entries(".", true)?;
+    let mut siv = Cursive::default();
+
+    let entries = get_entries(".", false)?;
+
+    let entry_names = entries
+        .iter()
+        .map(|file| file.file_name().to_str().unwrap().to_string())
+        .collect::<Vec<_>>();
 
     println!("{:?}", entries);
+    println!("{:?}", entry_names);
 
-    // siv.add_global_callback('q', |s| s.quit());
+    let select = Dialog::around(
+        SelectView::<String>::new()
+            // .item_str("world")
+            .with_all_str(entry_names)
+            .with_name("select")
+            .fixed_size((30, 20)),
+    )
+    .title(pwd.to_str().unwrap());
 
-    // siv.run();
+    siv.add_layer(select);
+
+    siv.add_global_callback('q', |s| s.quit());
+
+    siv.run();
 
     Ok(())
 }
