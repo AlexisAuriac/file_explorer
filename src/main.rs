03e7cc2d847ed50038fea696ef05f5e114e25aa7
alexis <alexis.auriac@epitech.eu>
2020-02-13 16:21:27 +0100

    Can change directory, can't go to parent directory

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,49 @@ use cursive::traits::*;
 use cursive::views::{Dialog, SelectView};
 use cursive::Cursive;
 
-fn get_entries(path: &str, hide: bool) -> Result<Vec<DirEntry>, io::Error> {
+fn get_cwd_content(path: &str, hide: bool) -> Result<Vec<DirEntry>, io::Error> {
     let entries = fs::read_dir(path)?.collect::<Result<Vec<_>, io::Error>>()?;
 
-    if hide {
-        Ok(entries
+    let entries = if hide {
+        entries
             .into_iter()
             .filter(|s| match s.file_name().to_str() {
                 Some(s) if !s.starts_with('.') => true,
                 _ => false,
             })
-            .collect::<Vec<_>>())
+            .collect::<Vec<_>>()
     } else {
-        Ok(entries)
-    }
+        entries
+    };
+
+    Ok(entries)
+}
+
+fn update_title(s: &mut Cursive) {
+    let pwd = PathBuf::from(".");
+    let pwd = fs::canonicalize(pwd).unwrap();
+    let mut dialog = s.find_name::<Dialog>("dialog").unwrap();
+
+    dialog.set_title(pwd.to_str().unwrap());
+}
+
+fn update_content(s: &mut Cursive) {
+    let entries = get_cwd_content(".", false).unwrap();
+    let entry_names = entries
+        .iter()
+        .map(|file| file.file_name().to_str().unwrap().to_string())
+        .collect::<Vec<_>>();
+    let mut select = s.find_name::<SelectView<String>>("select").unwrap();
+
+    select.clear();
+    select.add_all_str(entry_names);
+}
+
+fn change_dir(s: &mut Cursive, name: &str) {
+    std::env::set_current_dir(PathBuf::from(name)).unwrap();
+
+    update_title(s);
+    update_content(s);
 }
 
 fn main() -> io::Result<()> {
@@ -28,7 +57,7 @@ fn main() -> io::Result<()> {
 
     let mut siv = Cursive::default();
 
-    let entries = get_entries(".", false)?;
+    let entries = get_cwd_content(".", false)?;
 
     let entry_names = entries
         .iter()
@@ -40,12 +69,13 @@ fn main() -> io::Result<()> {
 
     let select = Dialog::around(
         SelectView::<String>::new()
-            // .item_str("world")
+            .on_submit(change_dir)
             .with_all_str(entry_names)
             .with_name("select")
             .fixed_size((30, 20)),
     )
-    .title(pwd.to_str().unwrap());
+    .title(pwd.to_str().unwrap())
+    .with_name("dialog");
 
     siv.add_layer(select);
 
